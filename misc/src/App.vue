166c28fbb6b7f6026ca2b896fe73e0e54c7facc2
sacred81 <sacred81@naver.com>
2022-11-21 00:10:45 +0900

    Add menus option
    Signed-off-by: sacred81 <sacred81@naver.com>

--- misc/src/App.vue
+++ misc/src/App.vue
@@ -1,18 +1,26 @@
 <template>
   <div id="app">
-    <ul>
-      <li><router-link :to="{ name: 'DaVincisLock' }">다빈치의 자물쇠</router-link></li>
-      <li><router-link :to="{ name: 'BachelorsOfPisa' }">피사의 학사</router-link></li>
-      <li><router-link :to="{ name: 'CleverMerchant' }">영리한 상인</router-link></li>
-      <li><router-link :to="{ name: 'LimaFestival' }">리마 축제</router-link></li>
-    </ul>
+    <div v-if="menus">
+      <ul>
+        <li><router-link :to="{ name: 'DaVincisLock' }">다빈치의 자물쇠</router-link></li>
+        <li><router-link :to="{ name: 'BachelorsOfPisa' }">피사의 학사</router-link></li>
+        <li><router-link :to="{ name: 'CleverMerchant' }">영리한 상인</router-link></li>
+        <li><router-link :to="{ name: 'LimaFestival' }">리마 축제</router-link></li>
+      </ul>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      menus: this.$route.query.menus,
+    };
+  },
 }
 </script>
 
